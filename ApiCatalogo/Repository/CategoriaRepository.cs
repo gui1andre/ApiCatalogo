aa903e66da955cff623594c060541717737c2605
Guilherme Andre <gui1andre89@gmail.com>
2023-06-13 11:38:56 -0300

    Add categoria

---
+++ ApiCatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Repository;
+
+namespace ApiCatalogo.Repository;
+
+public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+{
+	public CategoriaRepository(AppDbContext context) : base(context)
+	{
+
+	}
+
+    public IEnumerable<Categoria> GetCategoriasPorProduto()
+    {
+        return Get().Include(c => c.Produtos).ToList();
+    }
+}
+
---
+++ ApiCatalogo/Repository/ICategoriaRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ApiCatalogo.Models;
+using ToDoApi;
+
+namespace ApiCatalogo.Repository;
+
+public interface ICategoriaRepository : IRepository<Categoria>
+{
+    IEnumerable<Categoria> GetCategoriasPorProduto();
+}
+
